a15e169fe93c6dc8595bdfcabd495d5f2f80d344
Rajpriyar20 <rajpriyar2002@gmail.com>
2021-09-29 17:48:23 +0530

    "mongoose commit"

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
 const express = require("express");
-
+const mongoose = require("mongoose");
 //database
 const database = require("./database");
 
+mongoose.connect(
+        process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        }
+    ).then(() => console.log("connection established!"))
+    .catch((err) => {
+        console.log(err);
+    });
 //initialization
 const OurApp = express();
 
